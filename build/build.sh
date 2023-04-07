c4d11aff506bf343b8afcb59567849f57444cabd
Jon Rood <jon.rood@nrel.gov>
2023-04-07 10:52:52 -0600

    Add intel license copy.

--- build/build.sh
+++ build/build.sh
@@ -3,4 +3,5 @@ set -e
 export GOOSE_ROOT=$(readlink -f ../)
 export SPACK_ROOT=$(readlink -f ../spack)
 source ${SPACK_ROOT}/share/spack/setup-env.sh
+cp ${HOME}/save/intel/license.lic ${SPACK_ROOT}/etc/spack/licenses/intel/
 nice make -j4
