d4c142100c9a74fe501f6549a147865983439c1b
Jon Rood <jon.rood@nrel.gov>
2023-04-28 14:29:37 -0600

    Update Ellis config.

--- configs/ellis/env.sh
+++ configs/ellis/env.sh
@@ -2,12 +2,12 @@
 
 export NREL_CLUSTER=ellis
 
-export MODULE_PREFIX=/data/ssd1/software/2022-11-28/opt/bootstrap/linux-rocky8-zen/gcc-8.5.0/environment-modules-5.2.0-6q2w5qvdl4jzhod5uava5qr3c6d2fpel
+MODULES_DATE=2023-03
+export MODULE_PREFIX=/data/ssd1/software/${MODULES_DATE}/opt/bootstrap/linux-rocky8-zen/gcc-10.4.0/environment-modules-5.2.0-2joscpkp3qvte32ggyimdtjcoa2rceve
 export PATH=${MODULE_PREFIX}/bin:${PATH}
 module() { eval $(${MODULE_PREFIX}/bin/modulecmd $(basename ${SHELL}) $*); }
 
-MODULES_DATE=2022-11-28
 module purge
 module unuse ${MODULEPATH}
-module use /data/ssd1/software/${MODULES_DATE}/modules/compilers/linux-rocky8-zen/gcc-8.5.0
-module use /data/ssd1/software/${MODULES_DATE}/modules/packages/linux-rocky8-zen2/gcc-10.4.0
+module use /data/ssd1/software/${MODULES_DATE}/modules/compilers/linux-rocky8-zen/gcc-10.4.0
+module use /data/ssd1/software/${MODULES_DATE}/modules/software/linux-rocky8-zen2/gcc-10.4.0
