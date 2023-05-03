30c2c07a355314864f19e011792f9fe9f0f70fb1
Jon Rood <jon.rood@nrel.gov>
2023-05-03 14:24:29 -0600

    Try to make gcc a dependency of intel-parallel-studio.

---
+++ repos/nrel/packages/intel-parallel-studio/package.py
@@ -0,0 +1,6 @@
+from spack.pkg.builtin.intel-parallel-studio import IntelParallelStudio as bIntelParallelStudio
+
+class IntelParallelStudio(bIntelParallelStudio):
+    # Avoid error /lib64/libstdc++.so.6: version `CXXABI_1.3.9'
+    # not found (required by ./libplatform_picker_AXE_install.so)
+    depends_on("gcc@8:", type="build")
