4111d66bba76ffc996fcc8d37efb1431c46e4ff5
Jon Rood <jon.rood@nrel.gov>
2024-04-30 10:52:36 -0600

    Updates.

---
+++ repos/nrel/packages/visit/package.py
@@ -0,0 +1,6 @@
+from spack.pkg.builtin.visit import Visit as bVisit
+import os
+
+class Visit(bVisit):
+    version("3.4.1", sha256="942108cb294f4c9584a1628225b0be39c114c7e9e01805fb335d9c0b507689f5")
+    version("3.4.0", sha256="6cfb8b190045439e39fa6014dfa797de189bd40bbb9aa6facf711ebd908229e3")
