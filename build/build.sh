2c1d97a401fe381fe3f3f5e7062292301f885ad7
Jon Rood <jon.rood@nrel.gov>
2025-05-28 17:28:25 -0600

    More updates.

--- build/build.sh
+++ build/build.sh
@@ -4,8 +4,8 @@ set -e
 export SPACK_USER_CONFIG_PATH=${PWD}/.spack
 export SPACK_USER_CACHE_PATH=${PWD}/.spack
 export SPACK_DISABLE_LOCAL_CONFIG=true
-export GOOSE_ROOT=$(readlink -f ../)
-export SPACK_ROOT=$(readlink -f ../spack)
+export GOOSE_ROOT=$(readlink -f ..)
+export SPACK_ROOT=${GOOSE_ROOT}/spack
 source ${SPACK_ROOT}/share/spack/setup-env.sh
 rm -rf ${SPACK_ROOT}/var/spack/environments/software
 spack env create software ${GOOSE_ROOT}/configs/darwin/spack.yaml
