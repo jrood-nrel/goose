d5f726f3981521e9bdc9de7bf3c46269f14b530a
Jon Rood <jon.rood@nrel.gov>
2025-05-29 13:48:01 -0600

    Updates.

--- build/build.sh
+++ build/build.sh
@@ -1,14 +1,27 @@
 #!/bin/bash -l
+
+cmd() {
+  echo "+ $@"
+  eval "$@"
+}
+
+if [[ "${NREL_CLUSTER}" == 'ellis' ]]; then
+  cmd "export MY_MACHINE=ellis"
+  cmd "module purge" || true
+elif [[ "$(sw_vers --productName)" == 'macOS' ]]; then
+  cmd "export MY_MACHINE=darwin"
+fi
+
+cmd "export SPACK_USER_CONFIG_PATH=${PWD}/.spack"
+cmd "export SPACK_USER_CACHE_PATH=${PWD}/.spack"
+cmd "export SPACK_DISABLE_LOCAL_CONFIG=true"
+cmd "export GOOSE_ROOT=$(readlink -f ..)"
+cmd "export SPACK_ROOT=${GOOSE_ROOT}/spack"
+
 set -e
-#module purge || true
-export SPACK_USER_CONFIG_PATH=${PWD}/.spack
-export SPACK_USER_CACHE_PATH=${PWD}/.spack
-export SPACK_DISABLE_LOCAL_CONFIG=true
-export GOOSE_ROOT=$(readlink -f ..)
-export SPACK_ROOT=${GOOSE_ROOT}/spack
-source ${SPACK_ROOT}/share/spack/setup-env.sh
-rm -rf ${SPACK_ROOT}/var/spack/environments/software
-spack env create software ${GOOSE_ROOT}/configs/darwin/spack.yaml
-spack env activate software
-spack concretize -f
-time nice spack install
+cmd "source ${SPACK_ROOT}/share/spack/setup-env.sh"
+cmd "rm -rf ${SPACK_ROOT}/var/spack/environments/software"
+cmd "spack env create software ${GOOSE_ROOT}/configs/${MY_MACHINE}/spack.yaml"
+cmd "spack env activate software"
+cmd "spack concretize -f"
+cmd "time nice spack install"
