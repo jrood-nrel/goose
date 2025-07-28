11c0790ed925cb44a200d3df7501492cdae3adc7
Jon Rood <jon.rood@nrel.gov>
2025-07-28 11:47:44 -0600

    Updates.

--- build/build2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-set -e
-module purge || true
-export GOOSE_ROOT=$(readlink -f ../)
-export SPACK_ROOT=$(readlink -f ../spack)
-source ${SPACK_ROOT}/share/spack/setup-env.sh
-spack clean -mps
-time nice make -j
