d9dbdbb53419459de3c7005c06f78af03dd0b854
Jon Rood <jon.rood@nrel.gov>
2025-09-03 14:15:14 -0600

    Fix llvm python dependency.

--- repos/spack_repo/exawind/packages/llvm/package.py
+++ repos/spack_repo/exawind/packages/llvm/package.py
@@ -5,3 +5,4 @@ from spack_repo.builtin.packages.llvm.package import Llvm as bLlvm
 
 class Llvm(bLlvm):
      patch("lldb_python_exe_relative_path.patch", when="@16:+lldb+python")
+     depends_on("python@3.8:3.12", when="@:19 +python")
