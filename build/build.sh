d229d125181bea9e3bb0bb0e7bfb562f1d4e4f40
Jon Rood <jon.rood@nrel.gov>
2025-09-08 14:17:09 -0600

    Fix spack timeout.

--- build/build.sh
+++ build/build.sh
@@ -4,6 +4,7 @@ module purge || true
 export GOOSE_ROOT=$(readlink -f ../)
 export SPACK_ROOT=$(readlink -f ../spack)
 source ${SPACK_ROOT}/share/spack/setup-env.sh
+sed -i 's/timeout=60/timeout=7200/' ${SPACK_ROOT}/lib/spack/spack/stage.py || true
 spack clean -mps
 make clean
 time nice make -j
